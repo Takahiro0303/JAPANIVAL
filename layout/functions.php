c26c2769eb70b1bcbe1593aeeb52e7b1f94c2445
takahiro tshuchida <taka020303@gamil.com>
2017-06-30 15:12:18 +0800

    functions.php中身変更

--- layout/functions.php
+++ layout/functions.php
@@ -1,17 +1,43 @@
 <?php 
+/* =====================================================
+    検証用関数
+====================================================- */
+    function v($var) {
+        echo '<pre>';
+        var_dump($var);
+        echo '</pre>';
+    }
 
-// サイトタイトル用関数
-function siteTitle(){
-	return 'JAPANIVAL';
-}
+    function e($var) {
+        echo $var;
+        echo '<br>';
+    }
 
+/* =====================================================
+    実機能関数
+====================================================- */
+    function $login_get_user($hoge) {
+        $sql = 'SELECT * FROM hogehoge WHERE fugafuga=?';
+        $data = [$_SESSION['id']];
+        $stmt = $hoge->prepare($sql);
+        $stmt->execute($data);
+        $login_user = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $login_user;
+    }
 
-function uri(){
-	$uri = $_SERVER['REQUEST_URI'];
+    function hs($var) {
+        return htmlspecialchars($var);
+    }
 
-    $uri_arr = explode('/', $uri);
-    $file_name = end($uri_arr);
-    return $file_name;
-}
+    function uri() {
+        $uri = $_SERVER['REQUEST_URI'];
+        $uri_arr = explode('/', $uri);
+        $file_name = end($uri_arr);
+        return $file_name;
+    }
 
- ?>
+    function h($var) {
+        header('Location:' . $var);
+        exit();
+    }
+?>
