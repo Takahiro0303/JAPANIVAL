bb01e35e975216b4a09d8d9ff9d4f74dd5868a3e
Taka.H <like.the.wind0306@gmail.com>
2017-06-30 15:56:20 +0800

    css読み込みコード書き換え

--- layout/partial/load_css.php
+++ layout/partial/load_css.php
@@ -1,28 +1,20 @@
- 	<!-- CSSの読み込み -->
- 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
- 	<link rel="stylesheet" type="text/css" href="assets/font-awsome/css/font-awsome.css">
- 	<!-- 独自CSSの読み込みエリア -->
- 	<link rel="stylesheet" type="text/css" href="assets/css/navbar.css">
- 	<!-- URLにより分岐させる
- 		例）読み込みファイル名がtimeline.phpの時 -->
-
-
+<!-- cssの読み込み -->
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.css">
+<!-- 独自cssの読み込みエリア -->
+<link rel="stylesheet" type="text/css" href="assets/css/navbar.css">
 
 <?php 
+    $uri = $_SERVER['REQUEST_URI'];
+    // スラッシュで配列化
+    // 配列の一番最後の値を取得
+    // ファイル名により分岐
 
-$uri = $_SERVER['REQUEST_URI'];
-// /20170508_Basic/layout_sample/index.php
-// var_dump($uri);
-// スラッシュ区切りで配列化
-// 配列の一番最後の値を取得
-// ファイル名により分岐
-
-$uri_arr = explode('/', $uri);
-var_dump($uri_arr);
-$file_name = end($uri_arr);
-
+    $uri_arr = explode('/', $uri);
+    $file_name = end($uri_arr);
  ?>
 
- <?php if ($file_name == 'timeline.php') : ?>
- 	<!-- timelineページのCSSの読み込み -->
- <?php endif ; ?>
+ <?php if($file_name == 'timeline.php'): ?>
+    <!-- timelineページのcss読み込み -->
+    <link rel="stylesheet" type="text/css" href="assets/css/timeline.css">
+ <?php endif; ?>
