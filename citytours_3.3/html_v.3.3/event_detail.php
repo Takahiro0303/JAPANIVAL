bd04095192cb233c0cb447bf68e3c5af31cd921e
Jollibee <gagaga.iina252@gmail.com>
2017-07-08 14:10:17 +0800

    events.phpの追加

--- citytours_3.3/html_v.3.3/event_detail.php
+++ citytours_3.3/html_v.3.3/event_detail.php
@@ -2,27 +2,12 @@
 session_start();
 require('../../common/dbconnect.php');
 require('../../common/functions.php');
+require('../../common/evemt.data.php');
 require('header.php');
 
-// if (!isset($_GET['event_id'])) {
-//     h('main_portal.php');
-// }else {
-
-// $event_id = $_GET['event_id'];
-// $sql = 'SELECT * FROM events WHERE event_id = ?';
-// $data = [$event_id];
-// $stmt = $dbh->prepare($sql);
-// $stmt->execute($data);
-// $event = $stmt->fetch(PDO::FETCH_ASSOC);
-// }
 
 
-// $reviews=array();
-// $sql = 'SELECT r.*,m.pic_path,m.f_name FROM `reviews` r,`members` m WHERE r.user_id=m.user_id AND event_id = ?';
-// $data = [$event_id];
-// $stmt = $dbh->prepare($sql);
-// $stmt->execute($data);
-// $reviews[] = $stmt->fetch(PDO::FETCH_ASSOC);
+
 
 ?>
 
