da71ce0be39cd22c9dbf4d876a2385e54f9c45e9
Jollibee <gagaga.iina252@gmail.com>
2017-07-20 10:25:51 +0800

    更新

--- citytours_3.3/html_v.3.3/event_detail.php
+++ citytours_3.3/html_v.3.3/event_detail.php
@@ -491,7 +491,7 @@ if (!empty($_POST['like_data'])) {
                     </div> -->
                     <a href="#" class="btn_1 add_bottom_30" data-toggle="modal" data-target="#myReview">Leave a review</a>
                     <p>
-                        <a class="btn_map" name="request" data-toggle="collapse" href="" data-text-swap="Cancel" data-text-original="Confirm to eve tomo">Confirm to eve tomo</a>
+                        <a class="btn_map" name="request" data-toggle="modal" href="#" data-text-swap="cancel" data-text-original="Confirm to eve tomo">Confirm to eve tomo</a>
                     </p>
 
                 </aside> <!-- class="col-md-4" -->
@@ -508,116 +508,59 @@ if (!empty($_POST['like_data'])) {
     </footer>
 
 <!-- Modal Review -->
-    <div class="modal fade" id="myReview" tabindex="-1" role="dialog" aria-labelledby="myReviewLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
-                    </button>
-                    <h4 class="modal-title" id="myReviewLabel">Write your review</h4>
-                </div>
-                <div class="modal-body">
-                    <div id="message-review">
-                    </div>
-                    <form method="post" action="assets/review_restaurant.php" name="review_restaurant" id="review_restaurant">
-                        <input name="restaurant_name" id="restaurant_name" type="hidden" value="Mexican Taco Mex">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <input name="name_review" id="name_review" type="text" placeholder="Your name" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <input name="lastname_review" id="lastname_review" type="text" placeholder="Your last name" class="form-control">
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End row -->
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <input name="email_review" id="email_review" type="email" placeholder="Your email" class="form-control">
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End row -->
-                        <hr>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>Position</label>
-                                    <select class="form-control" name="position_review" id="position_review">
-                                        <option value="">Please review</option>
-                                        <option value="Low">Low</option>
-                                        <option value="Sufficient">Sufficient</option>
-                                        <option value="Good">Good</option>
-                                        <option value="Excellent">Excellent</option>
-                                        <option value="Superb">Super</option>
-                                        <option value="Not rated">I don't know</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>Service</label>
-                                    <select class="form-control" name="service_review" id="service_review">
-                                        <option value="">Please review</option>
-                                        <option value="Low">Low</option>
-                                        <option value="Sufficient">Sufficient</option>
-                                        <option value="Good">Good</option>
-                                        <option value="Excellent">Excellent</option>
-                                        <option value="Superb">Super</option>
-                                        <option value="Not rated">I don't know</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End row -->
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>Price</label>
-                                    <select class="form-control" name="price_review" id="price_review">
-                                        <option value="">Please review</option>
-                                        <option value="Low">Low</option>
-                                        <option value="Sufficient">Sufficient</option>
-                                        <option value="Good">Good</option>
-                                        <option value="Excellent">Excellent</option>
-                                        <option value="Superb">Super</option>
-                                        <option value="Not rated">I don't know</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>Quality</label>
-                                    <select class="form-control" name="quality_review" id="quality_review">
-                                        <option value="">Please review</option>
-                                        <option value="Low">Low</option>
-                                        <option value="Sufficient">Sufficient</option>
-                                        <option value="Good">Good</option>
-                                        <option value="Excellent">Excellent</option>
-                                        <option value="Superb">Super</option>
-                                        <option value="Not rated">I don't know</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End row -->
-                        <div class="form-group">
-                            <textarea name="review_text" id="review_text" class="form-control" style="height:100px" placeholder="Write your review"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <input type="text" id="verify_review" class=" form-control" placeholder="Are you human? 3 + 1 =">
-                        </div>
-                        <input type="submit" value="Submit" class="btn_1" id="submit-review">
-                    </form>
-                </div>
-            </div>
-        </div>
+  <form method="POST" action="admin_user.php" enctype="multipart/form-data">
+   <div class="modal fade" id="myReview" tabindex="-1" role="dialog" aria-labelledby="myReviewLabel" aria-hidden="true">
+      <div class="modal-dialog">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                  </button>
+                  <h4 class="modal-title" id="myReviewLabel">Write your review</h4>
+              </div>
+              <div class="modal-body">
+                  <div id="message-review"></div>
+                  <form method="post" action="assets/review_tour.php" name="review_tour" id="review_tour">
+
+                      <!-- End row -->
+
+                      <!-- End row -->
+                      <div class="row">
+                          <div class="col-md-6">
+                              <div class="form-group">
+                                  <label>Position</label>
+                                  <select class="form-control" name="review_rating" id="position_review">
+                                      <option value="">Please review</option>
+                                      <option value="1">1</option>
+                                      <option value="2">2</option>
+                                      <option value="3">3</option>
+                                      <option value="4">4</option>
+                                      <option value="5">5</option>
+                                  </select>
+                              </div>
+                          </div>
+                      </div>
+                      <!-- End row -->
+
+                      <!-- End row -->
+                      <div class="form-group">
+                          <textarea name="review_comment" id="review_text" class="form-control" style="height:100px" placeholder="Write your review"></textarea>
+                      </div>
+                      <div class="form-group">
+                          <span class="btn btn-primary">
+                          <input type="file" name="review_pic_path" multiple>
+                          </span>
+                      </div>
+                      <div>
+                        <input type="hidden" name="event_id" value="" id="modal_event_id">
+                      </div>
+                      <div>
+                        <input class="btn btn-primary" type="submit">
+                      </div>
+                  </div>
+          </div>
+      </div>
     </div>
-    <!-- End modal review -->
+  </form>
 
     <!-- Modal Request form -->
     <div class="modal fade" id="myReview" tabindex="-1" role="dialog" aria-labelledby="myReviewLabel" aria-hidden="true">
