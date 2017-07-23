ec6c344c1ebbf007978697cd43c10c4ccf5ed953
takahiro tshuchida <taka020303@gamil.com>
2017-07-23 21:43:01 +0800

    マッチング時のプロフィールモーダル実装

---
+++ citytours_3.3/html_v.3.3/modal_profile.php
@@ -0,0 +1,116 @@
+<div class="modal fade" id="myprofile" tabindex="-1" role="dialog" aria-labelledby="myRequestLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+
+    <!-- <div class="col-md-7"> -->
+
+      <div class="panel panel-default">
+        <div class="panel-heading">  <h4 >User Profile</h4></div>
+        <div class="panel-body">
+
+          <div class="box box-info">
+
+            <div class="box-body">
+              <div class="col-sm-6">
+                <div  align="center"> <img alt="User Pic" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" id="profile-image1" class="img-circle img-responsive"> 
+
+                  <!-- <input id="profile-image-upload" class="hidden" type="file"> -->
+                  <div style="color:#999;" ></div>
+                  <!--Upload Image Js And Css-->
+
+
+
+
+
+
+
+                </div>
+
+                <br>
+
+                <!-- /input-group -->
+              </div>
+              <div class="col-sm-6">
+                <h4 style="color:#00b1b1;">COMMENT</h4></span>
+                <span><p><?php  ?></p></span>            
+              </div>
+              <div class="clearfix"></div>
+              <hr style="margin:5px 0 5px 0;">
+
+
+              <div class="col-sm-5 col-xs-6 tital " >USERNAME:</div><div class="col-sm-7 col-xs-6 "><?php  ?></div>
+              <div class="clearfix"></div>
+              <div class="bot-border"></div>
+
+              <div class="col-sm-5 col-xs-6 tital " >COUNTRY:</div><div class="col-sm-7"><?php  ?></div>
+              <div class="clearfix"></div>
+              <div class="bot-border"></div>
+
+              <div class="col-sm-5 col-xs-6 tital " >GENDER:</div><div class="col-sm-7"><?php  ?></div>
+              <div class="clearfix"></div>
+              <div class="bot-border"></div>
+
+              <div class="col-sm-5 col-xs-6 tital " >LEVEL OF JAPANEASE:</div><div class="col-sm-7"><?php  ?></div>
+
+              <!-- <div class="clearfix"></div>
+              <div class="bot-border"></div>
+
+              <div class="col-sm-5 col-xs-6 tital " >COMMENT:</div><div class="col-sm-7"><?php  ?></div>
+ -->
+<!--               <div class="clearfix"></div>
+              <div class="bot-border"></div>
+
+              <div class="col-sm-5 col-xs-6 tital " >Place Of Birth:</div><div class="col-sm-7">Shirdi</div>
+
+              <div class="clearfix"></div>
+              <div class="bot-border"></div>
+
+              <div class="col-sm-5 col-xs-6 tital " >Nationality:</div><div class="col-sm-7">Indian</div>
+
+              <div class="clearfix"></div>
+              <div class="bot-border"></div>
+
+              <div class="col-sm-5 col-xs-6 tital " >Relition:</div><div class="col-sm-7">Hindu</div>
+ -->
+
+              <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+
+          </div>
+
+
+        </div> 
+      </div>
+    <!-- </div>   -->
+    <script>
+      $(function() {
+        $('#profile-image1').on('click', function() {
+          $('#profile-image-upload').click();
+        });
+      });       
+    </script> 
+
+
+
+
+
+
+
+
+<!-- 
+  </div>
+</div> -->
+
+
+
+
+
+</div>
+</div>
+</div>
+
+<script src="js/bootstrap-datepicker.js"></script>
+<script src="js/bootstrap-timepicker.js"></script>
+}}
