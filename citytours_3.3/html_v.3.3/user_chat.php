e6864ff623f33807a5bb312442d9d2861eda0dff
Jollibee <gagaga.iina252@gmail.com>
2017-07-23 09:44:45 +0800

    user_chat.php　途中

--- citytours_3.3/html_v.3.3/user_chat.php
+++ citytours_3.3/html_v.3.3/user_chat.php
@@ -13,10 +13,12 @@ $login_user = get_login_user($dbh);
 // }
 
 // ○クリックされたユーザーのIDを一件取得
+if(isset($_REQUEST['chat_room_id'])){
 $chat_room_id = $_REQUEST['chat_room_id'];
+}
 
 // ○バリデーションのためにrequest_idとaccept_user_idを取得
-$sql ='SELECT request_id,accept_user_id
+$sql ='SELECT *
            FROM caht_rooms
            WHERE chat_room_id=?';
 $data = [$chat_room_id];
@@ -26,6 +28,19 @@ $chat_check=$stmt->fetch(PDO::FETCH_ASSOC);
   
 $request_id = $chat_check['request_id'];
 $accept_user_id = $chat_check['accept_user_id'];
+
+$sql ='SELECT c.*,u.*
+      FROM messages m,users u
+      WHERE m.user_id=u.user_id
+      AND m.chat_room_id=?';
+    $data = [$chat_room_id];
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+    $messages = [];
+    while ($message = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $messages[] = $message;
+    }
+
 
 // ○もし、ログインユーザーのidがchat_roomsテーブルのaccept_idかaccept_user_idと合えばメッセージ読み込み。そうでなければ、user_chat.php(チャットトップ)へ繊維
 if($_SESSION['id'] == $request_id || $_SESSION['id'] == $accept_user_id){
@@ -152,101 +167,166 @@ if (!empty($_POST['message'])) {
   <header>
     <!-- チャット全件表示 -->
     <aside class="col-md-3">
-      <h2 class="page-header">チャット一覧</h2>
+      <h3 class="page-header" align="center">チャット一覧</h3>
     </aside>
     <!-- 個人間チャット表示 -->
     <div class="col-md-6">
-      <h2 class="page-header">チャット</h2>
+      <h3 class="page-header" align="center">チャット相手の名前を表示</h3>
     </div>
     <!-- チャット全件表示 -->
-    <aside class="col-md-3">
-      <h2 class="page-header">イベント詳細</h2>
+    <aside class="col-md-3" align="center">
+      <h3 class="page-header">イベント詳細</h3>
     </aside>
   </header>
 
-  <div class="main_section" style="padding-top: 100px; ">
-      <div class='container'>
-          <div class="row">
-              <!-- チャット全件表示 -->
-              <aside class="col-md-3">
-                  <div class="row">
-                  
-                  </div>
-              </aside>
-              
-              <!-- 個人間チャット表示 -->
-              <div class="col-md-6">
-                  <div class="row">
-                    <div id="messages" style="overflow-y: auto; width: 100%; height: 500px;">
+  <main style="padding-top: 100px; ">
+    <!-- チャット全件表示 -->
+    <aside class="col-md-3">
+      <a type="button" >
+        <div class="row">
+          <div class="col-md-3">
+               <img src="" alt="User Picture" class="img-circle">
+          </div>
+          <div class="col-md-9">
+                <div>Jack Sparrow</div>
+                <strong class="pull-right">09:45AM</strong>
+                (123) 123-456
+          </div>
+        </div>
+      </a>
+      <hr>
+      <a type="button" >
+        <div class="row">
+          <div class="col-md-3">
+               <img src="" alt="User Picture" class="img-circle">
+          </div>
+          <div class="col-md-9">
+                <div>Jack Sparrow</div>
+                <strong class="pull-right">09:45AM</strong>
+                (123) 123-456
+          </div>
+        </div>
+      </a>
+      <hr>
+      <a type="button" >
+        <div class="row">
+          <div class="col-md-3">
+               <img src="" alt="User Picture" class="img-circle">
+          </div>
+          <div class="col-md-9">
+                <div>Jack Sparrow</div>
+                <strong class="pull-right">09:45AM</strong>
+                (123) 123-456
+          </div>
+        </div>
+      </a>
+      <hr>
 
-                    <?php foreach($messages as $message){ ?>
-                      <section class="comment-list">
-                        <!-- チャット相手からのメッセージ -->
-                        <?php if ($message['user_id'] != $_SESSION['id']) { ?>
-                          <article class="row">
-                            <div class="col-md-2 col-sm-2 hidden-xs">
-                              <figure class="thumbnail">
-                                <img class="img-responsive" src="../../users_pic/<?php echo $message['pic_path']; ?>" />
-                              </figure>
-                            </div>
-                            <div class="col-md-10 col-sm-10">
-                              <div class="panel panel-default arrow left">
-                                <div class="panel-body">
-                                  <div class="comment-post">
-                                    <p>
-                                      <?php echo $message['message']; ?>
-                                    </p>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </article>
-                        <?php }elseif($message['user_id'] == $_SESSION['id']){ ?>
-                        <!-- 自分が送ったメッセージ -->
-                        <article class="row">
-                          <div class="col-md-10 col-sm-10">
-                            <div class="panel panel-default arrow right">
-                              <div class="panel-body">
-                                <div class="comment-post">
-                                  <p>
-                                    <?php echo $message['message']; ?>
-                                  </p>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="col-md-2 col-sm-2 hidden-xs">
-                            <figure class="thumbnail">
-                              <img class="img-responsive" src="../../users_pic/<?php echo $login_user['pic_path']; ?>" />
-                            </figure>
-                          </div>
-                        </article>
-                        <?php }; ?>
-                      </section>
-                    <?php } ?>
-                    </div>
-                      <form method="POST" action="">
-                        <div class="panel-footer">
-                          <input id="btn-input" type="text" name='message' class="form-control input-sm chat_input" placeholder="type a message">
-                          <input type="hidden" name="user_id" value="<?php echo $_SESSION['login_user_id']; ?>">
-                          <p  align="right">
-                              <input type="submit" value="Send" class="btn btn-danger" id="btn-chat">
+    </aside>
+    
+    <!-- 個人間チャット表示 -->
+    <div class="col-md-6">
+        <div class="row">
+          <div id="messages" style="overflow-y: auto; width: 100%; height: 500px;">
+
+          <?php foreach($messages as $message){ ?>
+            <section class="comment-list">
+              <!-- チャット相手からのメッセージ -->
+              <?php if ($message['user_id'] != $_SESSION['id']) { ?>
+                <article class="row">
+                  <div class="col-md-2 col-sm-2 hidden-xs">
+                    <figure class="thumbnail">
+                      <img class="img-responsive" src="../../users_pic/<?php echo $message['pic_path']; ?>" />
+                    </figure>
+                  </div>
+                  <div class="col-md-10 col-sm-10">
+                    <div class="panel panel-default arrow left">
+                      <div class="panel-body">
+                        <div class="comment-post">
+                          <p>
+                            <?php echo $message['message']; ?>
                           </p>
                         </div>
-                      </form>
+                      </div>
+                    </div>
                   </div>
+                </article>
+              <?php }elseif($message['user_id'] == $_SESSION['id']){ ?>
+              <!-- 自分が送ったメッセージ -->
+              <article class="row">
+                <div class="col-md-10 col-sm-10">
+                  <div class="panel panel-default arrow right">
+                    <div class="panel-body">
+                      <div class="comment-post">
+                        <p>
+                          <?php echo $message['message']; ?>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2 col-sm-2 hidden-xs">
+                  <figure class="thumbnail">
+                    <img class="img-responsive" src="../../users_pic/<?php echo $login_user['pic_path']; ?>" />
+                  </figure>
+                </div>
+              </article>
+              <?php }; ?>
+            </section>
+          <?php } ?>
+          </div>
+            <form method="POST" action="">
+              <div class="panel-footer">
+                <input id="btn-input" type="text" name='message' class="form-control input-sm chat_input" placeholder="type a message">
+                <input type="hidden" name="user_id" value="<?php echo $_SESSION['login_user_id']; ?>">
+                <p  align="right">
+                    <input type="submit" value="Send" class="btn btn-danger" id="btn-chat">
+                </p>
               </div>
+            </form>
+        </div>
+    </div>
 
-              <!--　イベント詳細&ユーザー詳細　表示 -->
-              <aside class="col-md-3">
-                  <div class="row">
+    <!--　イベント詳細&ユーザー詳細　表示 -->
+    <aside class="col-md-3">
+      <div class=" table-responsive">
+          <table　class="table table-striped">
+            <thead>
+              <tr>
+                <th　colspan="2"><?php echo $event_data['e_name']?></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Date&Time:</td><td>あ</td>
+                <td>City:</td><td>あ</td>
+                <td>place:</td><td>あ</td>
+                <td>Web Page:</td><td>あ</td>
 
-                      
-                  </div>
-              </aside>
-          </div><!-- row -->
-      </div> <!-- container -->
-  </div><!-- main_section -->
+              </tr>
+              
+
+            </tbody>
+          </table>
+          <table>
+            <thead>
+              <tr>
+                <th>例年参加者</th><th><?php echo $event_data['e_name']?></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Date&Time:</td><td></td>
+                <td>City:</td><td></td>
+                <td>place:</td><td></td>
+                <td>Web Page:</td><td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    </aside>
+
+  </main><!-- main_section -->
 
 </body>
 </html>
