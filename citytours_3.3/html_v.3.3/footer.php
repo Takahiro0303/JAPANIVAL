02b27f08657476e126df665eb93479e0dd55fc82
Jollibee <gagaga.iina252@gmail.com>
2017-07-27 12:46:54 +0800

    about us / user policy モデール作成完了

--- citytours_3.3/html_v.3.3/footer.php
+++ citytours_3.3/html_v.3.3/footer.php
@@ -2,10 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-sm-10">
-                <h3>User Policy</h3>
-                <h3>Notation based on the Specified Commercial Transaction Act</h3>
-                <h3>Inquiry</h3>
-                <h3>Privacy Policy</h3>
+                <a href="#" class="show-submenu" data-toggle="modal" data-target="#modal_about_us"><h3>About us</h3></a>
+                <a href="#" class="show-submenu" data-toggle="modal" data-target="#modal_user_policy"><h3>User policy</h3></a>
+                <a href="#" class="show-submenu" data-toggle="modal" data-target="#modal_inquiry.php"><h3>Inquiry</h3></a>
+                <a href="#" class="show-submenu" data-toggle="modal" data-target="#modal_Privacy Policy"><h3>Privacy Policy</h3></a>
             </div>
             <div class="col-md-2 col-sm-2">
                 <h3>Settings</h3>
@@ -27,3 +27,16 @@
         </div><!-- End row -->
     </div><!-- End container -->
 </footer><!-- End footer -->
+
+
+    <!-- モーダル・about us -->
+    <?php require('modal_about_us.php'); ?>
+
+    <!-- モーダル・ユーザー規約 -->
+    <?php require('modal_user_policy.php'); ?>
+
+    <!-- モーダル・問い合わせ -->
+    <?php //('modal_inquiry.php'); ?>
+
+    <!-- モーダル・個人情報規約 -->
+    <?php require('modal_privacy_policy.php'); ?>
---
+++ citytours_3.3/html_v.3.3/modal_privacy_policy.php
@@ -0,0 +1,12 @@
+<div class="modal fade" id="modal_privacy_policy" tabindex="-1" role="dialog" aria-labelledby="privacy_policy" aria-hidden="true">
+      <div class="modal-dialog">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                  </button>
+                  <h4 class="modal-title" id="about_us">Privacy Policy</h4>
+              </div>
+              <div class="modal-body">
+              </div>
+      </div>
+</div>
